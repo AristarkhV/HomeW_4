6132d9255c65b343f05a3ec1410f280613b2fd41
UKiyanitsa <u.kiyanitsa@gmail.com>
2019-05-31 03:16:22 +0300

    first

--- src/App/Main.java
+++ src/App/Main.java
@@ -3,6 +3,11 @@ package App;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Palindrome newString = new Palindrome();
+        System.out.println(newString.isPalindrome("Madam, I'm Ad"));
+
+        Factorial number = new Factorial();
+        System.out.println(number.factorial(3));
     }
 }
---
+++ src/App/Palindrome.java
@@ -0,0 +1,24 @@
+package App;
+
+public class Palindrome {
+    public Palindrome() {
+
+    }
+
+    public static boolean isPalindrome(String currentString) {
+
+        if (currentString == null) {
+            return true;
+        }
+        StringBuilder bufferString = new StringBuilder();
+        String reverceString = new String();
+        String validator = "[^a-zA-Z0-9]";
+        bufferString.append(currentString.replaceAll(validator, "").toLowerCase());
+        currentString = bufferString.toString();
+        reverceString = bufferString.reverse().toString();
+        if (reverceString.equals(currentString)) {
+            return true;
+        }
+        return  false;
+    }
+}
